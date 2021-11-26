3750bb492de1722581567c5538772819a94b6575
kma952727 <kma952727@gmail.com>
2021-11-26 16:48:27 +0900

    first commit

---
+++ src/main/java/com/example/searchbusanshopapi/SearchBusanShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchbusanshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBusanShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchBusanShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/searchbusanshopapi/SearchBusanShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchbusanshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchBusanShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
